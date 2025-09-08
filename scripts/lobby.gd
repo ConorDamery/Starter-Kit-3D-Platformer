5f8a8625e47d595a26f8b4a35701d6cae2f8119a
Conor Damery <90935800+ConorDamery@users.noreply.github.com>
2025-09-08 21:45:21 +0200

    WIP improved UI theme

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -8,17 +8,22 @@ extends Control
 @export var lobby_name_le : LineEdit
 @export var lobby_addr_le : LineEdit
 
-@export var enet_host_btn : Button
-@export var enet_join_btn : Button
-@export var enet_start_btn : Button
+@export var host_btn : Button
+@export var join_btn : Button
+@export var start_btn : Button
+@export var add_btn : Button
 
-@export var enet_lobby_list : ItemList
-@export var enet_player_list : ItemList
+@export var lobby_list : ItemList
+@export var player_list : ItemList
+
+@export var map_label : Label
+@export var map_selection : OptionButton
 
 @export var error_dialog : AcceptDialog
 
-var lobby_list: Dictionary
-var selected_lobby_index := -1
+var _map_list: Dictionary
+var _lobby_list: Dictionary
+var _lobby_selected := -1
 
 func _ready() -> void:
 	
@@ -31,31 +36,39 @@ func _ready() -> void:
 	Online.game_error.connect(self._on_game_error)
 	Online.game_log.connect(self._on_game_log)
 	
-	enet_lobby_list.item_selected.connect(self._on_lobby_selected)
+	lobby_list.item_selected.connect(self._on_lobby_selected)
+	
+	var idx = 0
+	for map_name in Online.game_maps.keys():
+		map_selection.add_item(map_name)
+		_map_list[idx] = map_name
+		idx += 1
+	
+	map_selection.item_selected.emit(0)
 
 func _on_connection_failed():
 	connect_panel.show()
 	players_panel.hide()
-	enet_host_btn.disabled = false
+	host_btn.disabled = false
 
 func _on_connection_succeded():
 	connect_panel.hide()
 	players_panel.show()
 
 func _on_lobby_list_changed():
-	selected_lobby_index = -1
-	enet_join_btn.disabled = true
+	_lobby_selected = -1
+	join_btn.disabled = true
 	
 	lobby_list.clear()
-	enet_lobby_list.clear()
+	lobby_list.clear()
 	for lby_info in Online.lobbies.values():
-		var idx = enet_lobby_list.add_item("%s [%d/%d]" % [lby_info["lobby_name"], lby_info["lobby_num_players"], lby_info["lobby_max_players"]])
-		lobby_list[idx] = lby_info
+		var idx = lobby_list.add_item("%s [%d/%d]" % [lby_info["lobby_name"], lby_info["lobby_num_players"], lby_info["lobby_max_players"]])
+		_lobby_list[idx] = lby_info["id"]
 
 func _on_player_list_changed():
-	enet_player_list.clear()
+	player_list.clear()
 	for player_name in Online.players.values():
-		enet_player_list.add_item(player_name if player_name != Online.player_name else player_name + " (you)")
+		player_list.add_item(player_name if player_name != Online.player_name else player_name + " (you)")
 
 func _on_game_started():
 	get_tree().get_root().get_node("Lobby").hide()
@@ -70,50 +83,65 @@ func _on_game_ended():
 func _on_game_error(what: String):
 	error_dialog.dialog_text = what
 	error_dialog.popup_centered()
-	enet_host_btn.disabled = false
+	host_btn.disabled = false
 
 func _on_game_log(what: String):
 	print("GAME LOG: " + what)
 
 func _on_lobby_selected(index: int):
-	selected_lobby_index = index
-	enet_join_btn.disabled = selected_lobby_index < 0
+	_lobby_selected = _lobby_list[index]
+	join_btn.disabled = _lobby_selected < 0
 
-func _on_enet_host_pressed() -> void:
-	Online.create_enet_host(player_name_le.text, lobby_name_le.text)
-	connect_panel.hide()
-	players_panel.show()
-	enet_start_btn.disabled = false
+func _on_player_name_changed(new_text: String) -> void:
+	lobby_name_le.text = new_text + "'s Lobby"
+	lobby_name_le.text_changed.emit(lobby_name_le.text)
 
-func _on_enet_join_pressed() -> void:
-	if selected_lobby_index < 0:
-		# We use the explicit lobby address
-		var parts = lobby_addr_le.text.split(":")
-		var addr = parts[0]
-		var port = int(parts[1])
-		Online.create_enet_client(player_name_le.text, addr, port)
-		
-	else:
-		# Use the selected lobby info
-		var lby_info = lobby_list[selected_lobby_index]
-		Online.create_enet_client(player_name_le.text, lby_info["addr"], lby_info["port"])
+func _on_lobby_name_changed(new_text: String) -> void:
+	host_btn.disabled = new_text.is_empty()
+
+func _on_lobby_addr_changed(new_text: String) -> void:
+	add_btn.disabled = new_text.is_empty()
+
+func _on_host_pressed() -> void:
+	Online.create_host(player_name_le.text, lobby_name_le.text)
 	
 	connect_panel.hide()
 	players_panel.show()
+	map_label.show()
+	map_selection.show()
+	
+	start_btn.disabled = false
 
-func _on_enet_start_pressed() -> void:
+func _on_join_pressed() -> void:
+	if _lobby_selected < 0:
+		return
+	
+	# Use the selected lobby info
+	Online.create_client(player_name_le.text, _lobby_selected)
+	
+	connect_panel.hide()
+	players_panel.show()
+	map_label.hide()
+	map_selection.hide()
+
+func _on_start_pressed() -> void:
 	Online.start_game()
 
-func _on_enet_quit_pressed() -> void:
+func _on_quit_pressed() -> void:
 	Online.end_game()
 
 func _on_refresh_pressed() -> void:
 	Online.request_lobbies()
 
-func _on_lobby_name_changed(new_text: String) -> void:
-	enet_host_btn.disabled = new_text.is_empty()
+func _on_add_lobby_pressed() -> void:
+	var parts = lobby_addr_le.text.split(":")
+	if parts.size() != 2:
+		return
+	
+	var lby_addr = parts[0]
+	var lby_port = int(parts[1])
+	var lby_id = multiplayer.multiplayer_peer.generate_unique_id()
+	Online.lan_add_lobby(lby_id, lby_addr, lby_port, "Temp Lobby", 0, 0)
 
-func _on_lobby_addr_changed(new_text: String) -> void:
-	enet_join_btn.disabled = new_text.is_empty()
-	selected_lobby_index = -1
-	enet_lobby_list.deselect_all()
+func _on_map_selected(index: int) -> void:
+	Online.lobby_map = _map_list[index]
--- scripts/online.gd
+++ scripts/online.gd
@@ -1,19 +1,17 @@
 extends Node
 
-const BROADCAST_ADDR := "255.255.255.255"
-
 const MAX_PLAYERS = 8
+
+var game_maps := { "main":"res://scenes/main.tscn" }
 
 var session: Node3D
 var lobby_id := -1
 var lobby_name: String
-var lobby_port := -1
+var lobby_map: String
 
-var udp := PacketPeerUDP.new()
 var lobbies := {}
 var is_host := false
 
-var game_scene := "res://scenes/main.tscn"
 var player_name: String
 
 var players := {}
@@ -28,23 +26,22 @@ signal game_ended()
 signal game_error(what: String)
 signal game_log(what: String)
 
-func _ready() -> void:
+func _ready():
 	session = load("res://scenes/session.tscn").instantiate()
 	get_tree().get_root().add_child.call_deferred(session)
 	
 	initialize_backend()
 	setup_callbacks()
 
-func initialize_backend() -> void:
-	# Bind the socket to the fixed broadcast port
-	udp.set_broadcast_enabled(true)
-	var err = udp.bind(0)
-	if err != OK:
-		push_error("Failed to bind UDP port, err: ", err)
-	
-	print("Lobby discovery initialized")
+func _process(delta: float):
+	poll_events()
 
-func setup_callbacks() -> void:
+#region Online API
+
+func initialize_backend():
+	lan_initialize()
+
+func setup_callbacks():
 	
 	multiplayer.peer_connected.connect(
 		func(id: int):
@@ -74,27 +71,26 @@ func setup_callbacks() -> void:
 			end_game()
 	)
 
-func _process(delta: float) -> void:
-	# Poll network events
-	while udp.get_available_packet_count() > 0:
-		var pkt = udp.get_packet().get_string_from_utf8()
-		var sender_ip = udp.get_packet_ip()
-		var sender_port = udp.get_packet_port()
-		print("Receive packet")
-		
-		# If host: respond to queries
-		if is_host and pkt == "LOBBY_QUERY":
-			enet_handle_query(sender_ip, sender_port)
-		
-		# If client: handle lobby replies
-		elif pkt.begins_with("LOBBY_INFO:"):
-			enet_handle_lobby_info(sender_ip, pkt)
+func poll_events():
+	lan_poll_events()
+
+func create_host(new_player_name: String, new_lobby_name: String):
+	lan_create_host(new_player_name, new_lobby_name)
+
+func create_client(new_player_name: String, lobby_id: int):
+	lan_create_client(new_player_name, lobby_id)
+
+func request_lobbies():
+	lan_request_lobbies()
+
+func close_connection():
+	lan_close_connection()
 
 func start_game():
 	assert(multiplayer.is_server())
 	
 	# Call load_world on all clients
-	session.load_world.rpc(game_scene)
+	session.load_world.rpc(game_maps[lobby_map])
 	
 	# Iterate over our connected peer ids
 	for peer_id in players:
@@ -110,6 +106,8 @@ func end_game():
 	
 	game_ended.emit()
 
+# RPC Functions
+
 @rpc("call_local", "any_peer")
 func register_player(new_player_name: String):
 	var id = multiplayer.get_remote_sender_id()
@@ -123,7 +121,7 @@ func register_player(new_player_name: String):
 			var player = session.spawned_players[peer_id]
 			player.set_authority.rpc_id(id, peer_id)
 		
-		session.load_world.rpc_id(id, game_scene)
+		session.load_world.rpc_id(id, game_maps[lobby_map])
 		session.spawn_player(id)
 
 @rpc("call_local", "any_peer")
@@ -136,7 +134,47 @@ func unregister_player(id):
 	players.erase(id)
 	player_list_changed.emit()
 
-func create_enet_host(new_player_name: String, new_lobby_name: String):
+#endregion
+
+#region Steam Backend
+
+#endregion
+
+#region LAN Backend
+
+const LAN_BROADCAST_ADDR := "255.255.255.255"
+const LAN_BROADCAST_PORT_RANGE = 65535
+
+var lan_broadcast_socket := PacketPeerUDP.new()
+var lan_broadcast_retries = 1
+var lan_broadcast_delay = 0.05
+
+func lan_initialize():
+	# Bind the socket to the fixed broadcast port
+	lan_broadcast_socket.set_broadcast_enabled(true)
+	var err = lan_broadcast_socket.bind(0) # Bind to ephemeral port
+	if err != OK:
+		push_error("Failed to bind UDP port, err: ", err)
+	
+	print("Lobby discovery initialized")
+
+func lan_poll_events():
+	# Poll network events
+	while lan_broadcast_socket.get_available_packet_count() > 0:
+		var pkt = lan_broadcast_socket.get_packet().get_string_from_utf8()
+		var sender_ip = lan_broadcast_socket.get_packet_ip()
+		var sender_port = lan_broadcast_socket.get_packet_port()
+		print("Receive packet")
+		
+		# If host: respond to queries
+		if is_host and pkt == "LOBBY_QUERY":
+			lan_handle_query(sender_ip, sender_port)
+		
+		# If client: handle lobby replies
+		elif pkt.begins_with("LOBBY_INFO:"):
+			lan_handle_lobby_info(sender_ip, pkt)
+
+func lan_create_host(new_player_name: String, new_lobby_name: String):
 	var peer = ENetMultiplayerPeer.new()
 	var err = peer.create_server(0, MAX_PLAYERS)
 	if err != OK:
@@ -149,8 +187,7 @@ func create_enet_host(new_player_name: String, new_lobby_name: String):
 	lobby_id = peer.generate_unique_id()
 	
 	# Query the actual address & port the OS assigned
-	lobby_port = peer.host.get_local_port()
-	print("Lobby started on port: ", lobby_port)
+	print("Lobby started on port: ", peer.host.get_local_port())
 	
 	register_player.rpc_id(multiplayer.get_unique_id(), new_player_name)
 	player_name = players[multiplayer.get_unique_id()]
@@ -159,9 +196,16 @@ func create_enet_host(new_player_name: String, new_lobby_name: String):
 	
 	is_host = true
 
-func create_enet_client(new_player_name: String, ip_address, port: int):
+func lan_create_client(new_player_name: String, lobby_id: int):
+	if not lobbies.has(lobby_id):
+		return
+	
+	var lby_info = lobbies[lobby_id]
+	var addr = lby_info["addr"]
+	var port = lby_info["port"]
+	
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(ip_address, port)
+	peer.create_client(addr, port)
 	
 	multiplayer.set_multiplayer_peer(peer)
 	
@@ -169,47 +213,61 @@ func create_enet_client(new_player_name: String, ip_address, port: int):
 	register_player.rpc_id(multiplayer.get_unique_id(), new_player_name)
 	player_name = players[multiplayer.get_unique_id()]
 
-func request_lobbies(retries := 1, delay := 0.05):
+func lan_request_lobbies():
 	lobbies.clear()
 	lobby_list_changed.emit()
 	
-	for i in retries:
-		for port in 65535:
-			udp.set_dest_address(BROADCAST_ADDR, port)
-			udp.put_packet("LOBBY_QUERY".to_utf8_buffer())
-		await get_tree().create_timer(delay).timeout
+	for i in lan_broadcast_retries:
+		# Try all possible ports since we're using ephemeral ports (aka we don't know what port to use)
+		for port in LAN_BROADCAST_PORT_RANGE:
+			lan_broadcast_socket.set_dest_address(LAN_BROADCAST_ADDR, port)
+			lan_broadcast_socket.put_packet("LOBBY_QUERY".to_utf8_buffer())
+		await get_tree().create_timer(lan_broadcast_delay).timeout
 	
 	print("Broadcasted lobby query")
 
-func enet_handle_query(client_ip: String, client_port: int, retries := 1, delay := 0.05):
+func lan_handle_query(client_ip: String, client_port: int):
 	# Send a reply back to the querying client
-	var reply = "LOBBY_INFO:%d:%d:%s:%d:%d" % [lobby_id, lobby_port, lobby_name, players.size(), MAX_PLAYERS]
-	for i in retries:
-		udp.set_dest_address(client_ip, client_port)
-		udp.put_packet(reply.to_utf8_buffer())
-		await get_tree().create_timer(delay).timeout
+	var port = multiplayer.multiplayer_peer.host.get_local_port()
+	var reply = "LOBBY_INFO:%d:%s:%d:%d:%d" % [lobby_id, lobby_name, port, players.size(), MAX_PLAYERS]
+	for i in lan_broadcast_retries:
+		lan_broadcast_socket.set_dest_address(client_ip, client_port)
+		lan_broadcast_socket.put_packet(reply.to_utf8_buffer())
+		await get_tree().create_timer(lan_broadcast_delay).timeout
 	
 	print("Replied to query from: %s:%d" % [client_ip, client_port])
 
-func enet_handle_lobby_info(sender_ip: String, reply: String):
+func lan_handle_lobby_info(sender_ip: String, reply: String):
 	var parts = reply.split(":")
 	if parts.size() != 6:
 		return
 	
-	var id = int(parts[1])
-	if lobbies.has(id):
+	var lby_id = int(parts[1])
+	if lobbies.has(lby_id):
 		return
 		
 	print("Discovered lobby: ", reply, ", from: ", sender_ip)
 	
-	var port = int(parts[2])
-	var lby_name = parts[3]
+	var lby_name = parts[2]
+	var lby_port = int(parts[3])
 	var lby_num_players = int(parts[4])
 	var lby_max_players = int(parts[5])
-	lobbies[id] = { "id":id, "addr": sender_ip, "port":port, "lobby_name":lby_name, "lobby_num_players":lby_num_players, "lobby_max_players":lby_max_players }
+	
+	lan_add_lobby(lby_id, sender_ip, lby_port, lby_name, lby_num_players, lby_max_players)
+
+func lan_add_lobby(lby_id: int, lby_addr: String, lby_port: int, lby_name: String, lby_num_players: int, lby_max_players: int):
+	lobbies[lby_id] = {
+		"id":lby_id,
+		"addr": lby_addr,
+		"port":lby_port,
+		"lobby_name":lby_name,
+		"lobby_num_players":lby_num_players,
+		"lobby_max_players":lby_max_players
+	}
+	
 	lobby_list_changed.emit()
 
-func close_connection():
+func lan_close_connection():
 	lobbies.clear()
 	is_host = false
 	
@@ -217,6 +275,8 @@ func close_connection():
 		multiplayer.multiplayer_peer.close()
 	
 	multiplayer.multiplayer_peer = null
+
+#endregion
 
 # Utility functions
 
